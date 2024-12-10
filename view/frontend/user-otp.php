05f06ab458191a9480aa937dc83e5cb55347d7c0
hamzaoui45 <hamzabenyahia65@gmail.com>
2024-12-10 14:26:56 +0100

    new form

--- view/frontend/user-otp.php
+++ view/frontend/user-otp.php
@@ -17,7 +17,7 @@ if($email == false){
             background-color: #f0f8ff;
             margin: 0;
             padding: 0;
-            display: flex;
+            
             flex-direction: column;
             min-height: 100vh;
         }
@@ -116,48 +116,3 @@ if($email == false){
             margin-top: auto; /* Keeps footer at the bottom */
         }
     </style>
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 offset-md-4 form">
-                <form action="user-otp.php" method="POST" autocomplete="off">
-                    <h2 class="text-center">Code Verification</h2>
-                    <?php 
-                    if(isset($_SESSION['info'])){
-                        ?>
-                        <div class="alert alert-success text-center">
-                            <?php echo $_SESSION['info']; ?>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                    <?php
-                    if(count($errors) > 0){
-                        ?>
-                        <div class="alert alert-danger text-center">
-                            <?php
-                            foreach($errors as $showerror){
-                                echo $showerror;
-                            }
-                            ?>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                    <div class="form-group">
-                        <input class="form-control" type="number" name="otp" placeholder="Enter verification code" required>
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control button" type="submit" name="check" value="Submit">
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <footer>
-    <p>&copy; 2024 AgriPlate. All rights reserved.</p>
-</footer>
-    
-</body>
-</html>
