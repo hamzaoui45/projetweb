fd740d7bb46d0abc64f79fb6111329a8ad4625ed
hamzaoui45 <hamzabenyahia65@gmail.com>
2024-12-10 21:07:56 +0100

    hello

--- view/frontend/user-otp.php
+++ view/frontend/user-otp.php
@@ -5,14 +5,3 @@ if($email == false){
   header('Location: login.php');
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
